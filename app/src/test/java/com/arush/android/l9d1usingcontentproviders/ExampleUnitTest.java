47781336170f2a8abc17ac692af3b1d8d361bb8f
Arush Gupta <arushgupta.me@gmail.com>
2016-06-29 22:34:40 -0700

    Content Provider App

---
+++ app/src/test/java/com/arush/android/l9d1usingcontentproviders/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.arush.android.l9d1usingcontentproviders;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
